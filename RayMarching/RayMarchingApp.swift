de46d4932d8e4a0b71b6692d0a4dfb1c3959a1e3
Noah Pikielny <np299@cornell.edu>
2022-11-04 14:31:14 -0400

    Initial Commit

---
+++ RayMarching/RayMarchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayMarchingApp.swift
+//  RayMarching
+//
+//  Created by Noah Pikielny on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RayMarchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
